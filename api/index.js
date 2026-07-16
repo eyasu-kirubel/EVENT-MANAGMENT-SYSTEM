47d31745fc08e2c3dbf634fb117ee008ef1d71d1
meron <meronnegussie18@gmail.com>
2026-07-16 10:58:18 +0300

    define endpoints structure

--- api/index.js
+++ api/index.js
@@ -2,6 +2,26 @@ const express = require("express");
 
 const server = express();
 
+server.get("/login", (request, response) => {
+  //
+});
+
+server.get("/register", (request, response) => {
+  //
+});
+
+server.get("/buy-ticket/:event_id", (request, response) => {
+  const eventId = request.params.event_id;
+
+  response.send(eventId);
+  //
+});
+
+// AS ORGANIZER
+server.get("/post-ticket", (request, response) => {
+  //
+});
+
 server.get("/events", (request, response) => {
   response.json([
     { event_name: "Concert", startDate: "16-07-2025", endDate: "21-07-2025" },
@@ -11,7 +31,21 @@ server.get("/events", (request, response) => {
 server.listen(3000, () => console.log("Server is listening!"));
 
 /**
- * - list event /events
+ * - as a customer
+ * /events
+ * /login
+ * /register
+ * /book-ticket
+ * /pay
+ *
+ * - as organize
+ * /post-event
+ * /check-event
+ * /delete-event
+ *
+ * - as admin
+ * /approve-event
+ * /reject-event
  *
  *
  *  */
